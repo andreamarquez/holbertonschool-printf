b2282217a80fcec13e150b599d3ced9b5c60af91
andreamarquez <58274716+andreamarquez@users.noreply.github.com>
2024-11-26 17:20:48 +0100

    Raphbranch (#5)
    * changed the library name to main.h to match the project check
    
    * function more than 40 so we added a fifth function in the file printf.c to handle the for loop, because of that we had to reajust the library/ secondly in the library ive put the struct in first otherwise betty doesnt agree to call handler_t/everything is ok for betty.
    
    * delete binary
    
    ---------
    
    Co-authored-by: raphaeldott <raphael.dott@hotmail.com>

---
+++ main.h
@@ -0,0 +1,35 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+/* Structures */
+/**
+ * struct handler - Structure to map format specifiers to their corresponding
+ * print functions.
+ * @format_specifier: The format specifier character (ex., 'i' for integers,
+ *'s' for strings).
+ * @print_func: Pointer to the function that handles printing for the given
+ * format specifier. The function takes a va_list of arguments.
+ */
+typedef struct handler
+{
+	char format_specifier;
+	int (*print_func)(va_list *args, char *buffer, int *p_buffer_index);
+} handler_t;
+
+
+/* Function Prototypes */
+int percent_s(va_list *args, char *buffer, int *p_buffer_index);
+int percent_percent(va_list *args, char *buffer, int *p_buffer_index);
+int percent_di(va_list *args, char *buffer, int *p_buffer_index);
+int percent_c(va_list *args, char *buffer, int *p_buffer_index);
+void init_buffer(int *p_buffer_index);
+void append_to_buffer(char *buffer, int *p_buffer_index, char c);
+void flush_buffer(char *buffer, int *p_buffer_index);
+int _printf(const char *format, ...);
+int handle_formant(const char *format, handler_t *types, va_list *args,
+		char *buffer, int *p_buffer_index);
+
+
+#endif /* PRINT_H */
