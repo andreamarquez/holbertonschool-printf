15b0debd56b7071529a16591097d5ba05c853c03
andreamarquez <andreamarquezg.95@gmail.com>
2024-11-26 13:05:16 +0100

    adds comments

--- printf.c
+++ printf.c
@@ -10,6 +10,7 @@
  * the console.
  * @format: A character string that contains 0 or more directives such as
  * (%c, %s, %d, %i, %%).
+ *
  * Return: The quantity of characters printed except the null terminator.
  */
 int _printf(const char * const format, ...)
@@ -50,6 +51,7 @@ int _printf(const char * const format, ...)
 					break;
 				}
 			}
+			
 		}
 		else
 		{
@@ -65,11 +67,30 @@ int _printf(const char * const format, ...)
 	return (printed_chars);
 }
 
+
+/**
+ * init_buffer - Initializes the buffer index to 0.
+ * @p_buffer_index: Pointer to the buffer index to be initialized.
+ *
+ * This function sets the value pointed to by p_buffer_index to 0,
+ * effectively initializing the buffer index.
+ */
 void init_buffer(int *p_buffer_index)
 {
 	*p_buffer_index = 0;
 }
 
+/**
+ * flush_buffer - Writes the contents of the buffer to standard output
+ * and resets the buffer index.
+ * @buffer: The buffer containing the data to be written.
+ * @p_buffer_index: Pointer to the current index in the buffer.
+ *
+ * This function checks if there is any data in the buffer (i.e., if the
+ * buffer index is greater than 0). If there is data, it writes the
+ * contents of the buffer to standard output using the write system call
+ * and then resets the buffer index to 0.
+ */
 void flush_buffer(char *buffer, int *p_buffer_index) {
 	if (*p_buffer_index > 0) {
 		write(1, buffer, *p_buffer_index);
@@ -77,6 +98,19 @@ void flush_buffer(char *buffer, int *p_buffer_index) {
 	}
 }
 
+/**
+ * append_to_buffer - Appends a character to the buffer and flushes the buffer
+ *                    if it reaches its maximum size.
+ * @buffer: The buffer to which the character is appended.
+ * @p_buffer_index: Pointer to the current index in the buffer.
+ * @c: The character to append to the buffer.
+ *
+ * This function appends the character 'c' to the buffer at the position
+ * indicated by '*p_buffer_index'. If the buffer is full (i.e., 
+ * *p_buffer_index >= BUFFER_SIZE), it calls the flush_buffer function to 
+ * clear the buffer and reset the index. After appending the character, 
+ * the buffer index is incremented.
+ */
 void append_to_buffer(char *buffer, int *p_buffer_index, char c) {
 	if (*p_buffer_index >= BUFFER_SIZE) {
 		flush_buffer(buffer, p_buffer_index);
